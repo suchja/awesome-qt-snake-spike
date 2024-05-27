fb3e05192f30817891886b7506c4da0568fce7db
Jan (LernMoment.de) <jan@lernmoment.de>
2024-05-27 11:22:13 +0200

    closes app via exit menu click

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,15 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionE_xit, SIGNAL(triggered()), this, SLOT(exitGame()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::exitGame()
+{
+    QApplication::quit();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void exitGame();
+
 private:
     Ui::MainWindow *ui;
 };
